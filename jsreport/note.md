6b908b62fed46a179b3b80ff0537bd0ae9c26a93
ibiann <trungpham152000@gmail.com>
2022-03-31 11:57:38 +0700

    report 31.03.2022

--- jsreport/note.md
+++ jsreport/note.md
@@ -1,6 +1,6 @@
-* output
+* output (built-in)
 - innerHTML trả về mã html của phần tử hiện tại
-- document.write() đưa ra màn nhưng chủ yếu để test
+- document.write(); đưa ra màn nhưng chủ yếu để test
 - alert() thông báo pop-up
 - console.log() đưa ra màn trong f12
 
@@ -53,6 +53,50 @@
 - onload
 - onmouseover
 
+////////////////////////////////////////////////////////////////////////
 * String 
 - length: //let length = text.length;
 - Strings as Objects
+- String parts: slice, substring, substr
+- text replace: replace()
+- String search: indexOf(), lastIndexOf(), startsWith(), endsWith()
+
+* Numbers
+- float
+- thêm kiểu dạng numbers -> Numbers
+- thêm kiểu dạnng string "" -> string ""
+- thêm kiểu dạng string "" + numbers -> string ""
+- "NaN"
+- "Infinity"
+
+* Number methods
+- toString(): number to string
+- toExponential(): số mũ
+- toFixed(): làm tròn console.log(x.toFixed(2));
+- toPrecision(): độ chính xác console.log(x.toPercision(2));
+
+- valueOf() numbers trả numbers console.log((1.5 + 8.0).valueOf());
+
+* Mảng
+- Methods:
++ pop() xoá 1 biến cuối trong mảng
++ push() thêm 1 biến vào mảng
++ objarray.shift();
++ objarray.unshift(); thêm một biến vào mảng và "unshift"
+
++ sử dụng length để append 1 biến mới vào array (easy-way method)
+
++ sử dụng concat() để merge hai mảng thành 1 mảng mới
+
++ sử dụng mảng splice() chứa 2 parameter [parameter 1 để add vị trí, parameter 2 để xem bao nhiêu elements sẽ xoá]
+
++ toString() trả mảng ra màn kèm phẩy
+
+- Sort: sắp xếp thứ tự (sort; reversing)
++ Numeric: sắp xếp stt
++ Compare function: xếp theo abc hay number order
++ có sử dụng toán max, min nhưng chưa học sâu cái này nâng cao nhưng có xem qua
+
+
+- Iteration: 
++ forEach(): liệt kê với cú pháp ngắn hơn
--- jsreport/test.js
+++ jsreport/test.js
@@ -1,9 +1,142 @@
-function myFunction() {
-    let text;
-    if (confirm("Press the button") == true) {
-        text = "Done";
-    } else {
-        text = "Bruh";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
+/**function */
+// function myFunction() {
+//     let text;
+//     if (confirm("Press the button") == true) {
+//         text = "Done";
+//     } else {
+//         text = "Bruh";
+//     }
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+// function myFunction() {
+//     let text = 
+//     document.getElementById("demo").innerHTML;
+//     document.getElementById("demo").innerHTML = text.replace("Trung","React");
+// }
+
+// var fullName;
+
+// fullName = 'Pham Chi Trung';
+
+// document.write(fullName + ' ' + 'Huh')
+
+
+// let x = "100";
+// let y = "10";
+// let z = x / y;
+// console.log(z);
+
+// Numbers
+// let x = 100;
+// let y = new Number (100);
+
+// document.getElementById("demo").innerHTML = (x==y);
+
+/**Array shift */
+// const newList = ["Trung", "React", "Bruh"]
+
+// console.log(newList.shift());
+
+// console.log(newList);
+
+/* unshift */
+
+// const newList = ["Trung", "React", "Bruh"];
+
+// console.log(newList.unshift("JavaScript"));
+
+// console.log(newList.unshift);
+
+
+/* Array Length */
+
+// const newArray = ["Trung", "React", "Bruh"];
+
+// document.getElementById("demo").innerHTML = newArray;
+// newArray[newArray.length] = "JavaScript";
+
+// document.getElementById("demo").innerHTML = newArray;
+
+/* concat() */
+
+// const oldList = ["Trung", "React", "Bruh"];
+
+// const newList = ["JavaScript", "Ruby", "PHP"];
+
+// const mergeList = oldList.concat(newList);
+
+// console.log(mergeList);
+
+/* splice */
+
+// const oldList = ["Trung", "React", "Bruh"];
+
+// document.getElementById("demo").innerHTML = oldList;
+// oldList.splice(1, 0, "JavaScript", "Ruby");
+
+// console.log(oldList);
+
+/* toString() */
+
+// const oldList = ["Trung", "ReReact", "Bruh"]
+
+// document.getElementById("demo").innerHTML = oldList.toString();
+
+/* Sort Numeric */
+
+// const points = [1, 2, 9, 4, 5, 6, 7, 11];
+
+// document.getElementById("demo").innerHTML = points;
+
+// points.sort(function(a,b){return a - b});
+
+// console.log(points)
+
+/* compare function */
+
+// const points = [1, 11, 4, 7, 9, 34, 90, 6];
+
+// console.log(points);
+
+// function myFunction1() {
+//     points.sort();
+//     document.getElementById("demo").innerHTML = points;
+// }
+
+// function myFunction2() {
+//     points.sort(function(a, b){return a - b });
+//     document.getElementById("demo").innerHTML = points;
+// }
+
+
+/* high and low array values */
+
+// const points = [1, 11, 4, 7, 9, 34, 90, 6];
+// points.sort(function(a,b){return a - b});
+
+// document.getElementById("demo").innerHTML = points[2];
+
+/* Iteration */
+/* forEach() */
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+
+// duyệt từng phần tử của mảng để cộng dồn vào biến sum
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log(sum); // 55
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let sum = 0;
+
+forEach(numbers, function (1) {
+    sum += 1;
+});
+
+console.log(sum);
+
