6b2035460004fa25d6d6c89404e7e6622c5ac5a3
ibiann <trungpham152000@gmail.com>
2022-04-01 15:00:57 +0700

    report 01.04.2022

--- jsreport/note.md
+++ jsreport/note.md
@@ -97,6 +97,78 @@
 + Compare function: xếp theo abc hay number order
 + có sử dụng toán max, min nhưng chưa học sâu cái này nâng cao nhưng có xem qua
 
+////////////////////////////////////////////////////////////////////////
 
 - Iteration: 
 + forEach(): liệt kê với cú pháp ngắn hơn
++ map(): biến đổi các phần tử của nó và đưa các kết quả vào một mảng mới.
++ filter(): kiểm tra với điều kiện
++ reduce(): The total ,The item value, The item index, The array itself
++ reduceRight(): lấy giá trị từ phải sang trái
++ every(): kiểm tra các phần tử của mảng có hợp điều kiện không
++ some(): lặp qua các phần tử của mảng, mỗi lần lặp sẽ truyền giá trị vào hàm callback
+
+* Date (không quan trọng thời gian hiện tại)
+- sử dụng get (d.get)
+- sử dụng set (d.set)
+
+* Boolean: mệnh đề true/false 
+ 
+
+* condition: mệnh đề so sánh if/else/else if/ switch case
+
+* Switch: chọn case để thực hiện <=> với biến cho xong xử lí từng case
+nếu case không có và gán default thì giá trị trả màn là default
+
+* Loop: for; for in; for/of; while; do/while
+- For...in dùng để loop 1 obj
+// for (var prop in obj) {
+//     console.log('obj.' + prop + '=' +obj[prop]);
+// };
+
+- for...of duyệt các đối tượng từ arr,str,map,...
+
+var myString = 'Trung';
+
+// for (var value of myString) {
+//     console.log(value);
+// }
+
+- while: Câu lệnh while tạo ra một vòng lặp thực thi một khối lệnh cho đến khi điều kiện vẫn đúng.
+
+- do...while: gần giống whiile nhưng code sẽ chạy ít nhất 1 lần nếu điều kiện thoả mãn
+
+* set: cho phép lưu trữ dữ liệu 1 cách không trùng lặp
+- new Set([""]); 
+- add(): object.add(variable);
+
+* map: là một tập hợp các cặp key/value có thể sử dụng mọi kiểu dữ liệu làm key và có thể duy trì thứ tự các phần tử của nó. 
+- new Set([""]); 
+- add(): object.add(variable);
+size : có thuộc tính size, Objects không có tích hợp để lấy ra size của nó.
+vòng lặp : Map có thể lặp lại trực tiếp, còn Object thì không.
+tính linh hoạt : Maps có thể có bất kì kiểu dữ liệu nào làm key cho 1 value, còn Object thì chỉ là chuỗi.
+
+* typeof : ktra dữ liệu của biến nào đó
+
+* error, try, catch, finally
+- error: tạo bởi coder
+
+- throws: khi có lỗi, js dừng đưa ra màn error message
+- finally để thực hiện code sau try với catch 
+
+* Strict mode
+- giúp code tốt hơn
+
+* this
+- đại diện cho 1 đối tượng
+
+- borrowing: với bind() method, 1 obj có thể mượn 1 method từ obj khác
+- this trong cú pháp
+- this dứng 1 mình = global trả [objectWindow]
+- vẫn cần tìm hiểu thêm
+* arrow function
+- làm code ngắn gọn
+
+* class
+- luôn thêm constructor() {}
--- jsreport/test.js
+++ jsreport/test.js
@@ -1,3 +1,4 @@
+"use strict";
 /**function */
 // function myFunction() {
 //     let text;
@@ -130,13 +131,298 @@
 
 // console.log(sum); // 55
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/* map() */
 
-let sum = 0;
+// var newList = [
+//     'Trung',
+//     'React',
+//     'Ruby'
+// ];
 
-forEach(numbers, function (1) {
-    sum += 1;
-});
+// newList.map(function(newList) {
+//     console.log(newList);
+// });
 
-console.log(sum);
+// var htmls = newList.map(function (newList) {
+//     return "<h2>".concat(newList, "</h2>");
+// });
+
+
+// console.log(htmls);
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// const numbers2 = numbers.map(myFunction);
+
+// console.log(numbers2);
+
+// function myFunction(value){
+//     return value * 2;
+// }
+
+/* filter */
+// const numbers = [10, 15, 20, 25, 49];
+
+// const numbersLarger = numbers.filter(myFunction);
+
+// console.log(numbersLarger);
+
+
+// function myFunction (value, index, array) {
+//     return value > 18;
+// }
+
+/* Reduce */
+
+// const numbers = [90, 20, 25, 30, 45];
+
+// let sum = numbers.reduce(myFunction);
+
+// console.log("The sum is:" + " " + sum);
+
+// function myFunction(total, value, index, array) {
+//     return total + value;
+// }
+
+/* every() */
+
+// const numbers = [10, 15, 20, 25, 49];
+
+// let numCon = numbers.every(myFunction);
+
+// console.log(numCon);
+
+// function myFunction (value) {
+//     return value > 18;
+// }
+
+// const smallerNum = (currentValue) => currentValue < 18;
+
+
+// const arr1 = [10, 15, 20, 25, 49];
+
+// console.log(arr1.every(smallerNum));
+
+
+/* some */
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let numCon = numbers.some(myFunction);
+
+// console.log(numCon);
+
+// function myFunction (value) {
+//     return value > 18;
+// }
+
+
+/* Switch */
+// var date = 6;
+
+// switch (date) {
+//   case 2:
+//     console.log('Today is Monday');
+//     break;
+
+//   case 3:
+//     console.log('Today is Monday');
+//     break;
+
+//   case 4:
+//     console.log('Today is Monday');
+//     break;
+
+//   case 5:
+//     console.log('Today is Thursday');
+//     break;
+//     default:
+//         console.log('Today is Non');
+// }
+
+
+/* for in */
+// var obj = {name: 'Trung', age: 22, major: "ICT"};
+
+// for (var prop in obj) {
+//     console.log('obj.' + prop + '=' +obj[prop]);
+// };
+
+/* for of */
+
+// var myString = 'Trung';
+
+// for (var value of myString) {
+//     console.log(value);
+// }
+
+// var myInfo = 'Trung';
+
+// for (var value of myInfo) {
+//     console.log(value);
+// }
+
+/* while */
+
+// var i = 0;
+
+// var n = 1;
+
+// while (i < 3) {
+//     console.log(i);
+//     i++;
+//     i += n;
+// }
+
+
+/* do while */
+
+// var result = '';
+
+// var i = 0;
+
+// do {
+//     i += 1;
+//     result += i + ' ';
+
+// }
+// while ( i > 0 && i < 5 && i == 1);
+
+// console.log(result);
+
+
+// var i = 10;
+// var thisSucces = false;
+
+// do {
+//     i++;
+
+//     console.log(' ' + i);
+//     if (true) {
+//         thisSucces = true;
+
+//     }
+// }
+// while (!thisSucces && i < 10);
+
+
+
+/* set */
+
+// const animals = new Set(["dog", "cat", "mouse"]);
+
+// console.log(animals.size);
+
+// console.log(animals.add("bird"));
+
+/* error */
+
+// try {
+//     adddlert("Welcome");
+// }
+
+// catch(err) { 
+//     console.log(err.message);
+// }
+
+// function myFunction() {
+//     const message = document.getElementById("p01");
+
+//     message.innerHTML = "";
+
+//     let x = document.getElementById("demo").value;
+//     try {
+//         if(x == " ") throw "is empty";
+//         if(isNaN(x)) throw "is not a number";
+
+//         x = Number(x);
+//         if(x > 10) throw "is too high";
+//         if(x < 5) throw "is too low";
+//     }
+//     catch(err) {
+//         message.innerHTML = "Input " + err;
+//     }
+//     finally {
+//         document.getElementById("demo").value = "";
+//     }
+// }
+
+/* this */
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 1000,
+//     fullName: function() { 
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(person.fullName());
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 1000,
+//     fullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// const subPerson = {
+//     firstName: "Hege",
+//     lastName: "Nilsen"
+// }
+
+// let fullName = person.fullName.bind(subPerson);
+
+// document.getElementById("demo").innerHTML = fullName();
+
+/* class */
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+// }
+//     const myCar = new Car("BWM", 2015);
+// console.log(myCar.name + " " + myCar.year);
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+
+
+// age() {
+//     let date = new Date ();
+//     return date.getFullYear() - this.year;
+// }
+// }
+
+// let myCar = new Car ("BWM", 2015);
+
+// console.log(myCar.name + " " + myCar.year);
+
+// console.log("My car is " + myCar.age() + " years old.");
+
+//Send parameter 
+
+// class Car {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+
+// age(x) {
+//     return x - this.year;
+//     }
+// }
+
+// let date = new Date();
+// let year = date.getFullYear();
+
+// let myCar = new Car("BWM", 2015);
+// document.getElementById("demo").innerHTML= "My car is " + myCar.age(year) + " years old.";
 
